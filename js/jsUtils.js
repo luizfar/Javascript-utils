95ea776dafd214f3506c1e2485c6d1f365adc4b0
Luiz Ribeiro <luizfar@gmail.com>
2010-10-11 12:33:27 -0300

    Moving functions to jsUtils object

---
+++ js/jsUtils.js
@@ -0,0 +1,39 @@
+var jsUtils = function() {
+  
+  function copyTitleToValueIfEmpty(element) {
+    text = jQuery.trim(element.val());
+    if (text == "" || text == element.attr("title")) {
+      element.val(element.attr("title")).addClass("fadeFieldColor");
+    } else {
+      element.removeClass("fadeFieldColor");
+    }
+  }
+  
+  function clearIfHasTitle(element) {
+    if (element.val() == element.attr("title")) {
+      element.val("").removeClass("fadeFieldColor");
+    }
+  }
+  
+  return {
+    bindWithTitle: function(elements) {
+      jQuery.each(elements, function() {
+          copyTitleToValueIfEmpty($(this));
+          var element = $(this);
+          $(element.parents("form:first")).submit(function() {
+            clearIfHasTitle(element)
+          })
+        })
+        .change(function() {
+          copyTitleToValueIfEmpty($(this));
+        })
+        .blur(function() {
+          copyTitleToValueIfEmpty($(this));
+        })
+        .focus(function() {
+          clearIfHasTitle($(this))
+        });
+    }
+  };
+
+}();
